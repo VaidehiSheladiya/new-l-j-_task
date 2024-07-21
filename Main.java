6321abe44fe3fe1a87dda2f15b61a57f0e91852e
VaidehiSheladiya <140635522+VaidehiSheladiya@users.noreply.github.com>
2024-07-21 22:21:41 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,48 @@
+
+
+
+import java.util.Scanner;
+class Food {
+    int code;
+    String foodName;
+    String sticker;
+    String foodType;
+    void getType() {
+        switch (sticker.toLowerCase()) {
+            case "green":
+                foodType = "Vegetarian";
+                break;
+            case "yellow":
+                foodType = "Contains Egg";
+                break;
+            case "red":
+                foodType = "Non Vegetarian";
+                break;
+            default:
+                foodType = "Unknown";
+                break;
+        }
+    }
+    void foodIn() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter Food Code: ");
+        code = scanner.nextInt();
+        scanner.nextLine();
+        System.out.print("Enter Food Name: ");
+        foodName = scanner.nextLine();
+        System.out.print("Enter Sticker");
+        sticker = scanner.nextLine();
+        getType();
+    }
+
+    void foodOut() {
+        System.out.println("Food Code: " + code);
+        System.out.println("Food Name: " + foodName);
+        System.out.println("Sticker: " + sticker);
+        System.out.println("Food Type: " + foodType);
+    }public static void main(String[] args) {
+        Food foodItem = new Food();
+        foodItem.foodIn();
+        foodItem.foodOut();
+    }
+}
